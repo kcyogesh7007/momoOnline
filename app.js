f5d80684c1db603760bc0bf6a2369b9d600b8366
Yogesh khadka <kcyogesh7007@gmail.com>
2026-02-21 14:31:30 +0545

    Add login and register

--- app.js
+++ app.js
@@ -1,11 +1,69 @@
 const express = require("express");
 const app = express();
-
 require("dotenv").config();
+const connectDB = require("./database/database");
+const User = require("./models/userModel");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-app.get("/", (req, res) => {
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+connectDB();
+
+app.post("/register", async (req, res) => {
+  const { email, password, phoneNumber, userName } = req.body;
+  if (!email || !password || !phoneNumber || !userName) {
+    return res.status(400).json({
+      message: "Please provide email,password,phoneNumber and userName",
+    });
+  }
+  const userExist = await User.findOne({ userEmail: email });
+  if (userExist) {
+    return res.status(400).json({
+      message: "User with that email already exist",
+    });
+  }
+
+  await User.create({
+    userEmail: email,
+    userPhoneNumber: phoneNumber,
+    userPassword: await bcrypt.hash(password, 10),
+    userName,
+  });
+  res.status(201).json({
+    message: "User registered successfully",
+  });
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({
+      message: "Please provide email and password",
+    });
+  }
+  const userExist = await User.findOne({ userEmail: email });
+  if (!userExist) {
+    return res.status(404).json({
+      message: "User with that email doesnot exist",
+    });
+  }
+  const isPasswordMatch = await bcrypt.compare(
+    password,
+    userExist.userPassword,
+  );
+  if (!isPasswordMatch) {
+    return res.status(400).json({
+      message: "Invalid credentials",
+    });
+  }
+  const token = jwt.sign({ id: userExist._id }, process.env.SECRET_KEY, {
+    expiresIn: "7d",
+  });
   res.status(200).json({
-    message: "API IS LIVE",
+    message: "User loggedIn successfully",
+    token,
   });
 });
 
---
+++ models/userModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: [true, "userName must be provided"],
+    },
+    userPassword: {
+      type: String,
+      required: [true, "userPassword must be provided"],
+    },
+    userPhoneNumber: {
+      type: String,
+      required: [true, "userPhoneNumber must be provided"],
+    },
+    userEmail: {
+      type: String,
+      required: [true, "userEmail must be provided"],
+    },
+    role: {
+      type: String,
+      enum: ["customer", "admin"],
+      default: "customer",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
