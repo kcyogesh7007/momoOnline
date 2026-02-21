0e83fa22928dced3127c2d3a9e56b344424c74dc
Yogesh khadka <kcyogesh7007@gmail.com>
2026-02-21 14:15:00 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "API IS LIVE",
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
